c412e3a9507159ba4cdb0f9f4ef4af7674b5c7a5
Matt Tyler <tylma141@student.otago.ac.nz>
2023-10-10 08:46:31 +1300

    More work on the merge

--- src/lib/merge.js
+++ src/lib/merge.js
@@ -82,29 +82,71 @@ class HeapNode {
 
 
 class Merge {
+
     // document_lists = [[uuid,uuid],[uuid,uuid]]
-    constructor(document_lists, database) {
+    constructor(document_lists, group_by = 1, database) {
         // needs to use an array of category lists
-        // init document_lists:
-        //      Sort by length from longest to shortest.
-        //      condense each list of documents by some number, 
+        // init document_lists:     
+        //      condense each list of documents by some number,
+        //      Sort by length from shortest to longest.
         //      grouping each eq class by that num.
+        this.document_lists = document_lists.reduce((acc, _, i, arr) => i % group_by === 0 ? [...acc, arr.slice(i, i + group_by).flat()] : acc, [])
+            .sort((a, b) => a.length - b.length)
+            .map((doc_list) => new HeapNode().equivalenceClass = doc_list)
+
         //      
         // Picks the longest as the main list to sort into.
+        this.main_list = document_lists.pop();
+
+        // Takes the next largest as the sort list
+        this.sort_list = document_lists.pop();
+
         // Then sets an upper bound and a lower bound
-        // Takes the top element off of the sort list and binary sort into the main list
-        // Update the upper bound with new upper bound
-        // Sets new upper bound. 
-        // Takes the bottom element off of the sort list and binary sort into the main list. 
-        // Sets new lower bound
-        // Repeat until sorted in and apply to next list etc. 
-
-
+        this.upper_bound = this.main_list.length - 1;
+        this.lower_bound = 0;
     }
 
     static fromObject() { }
     toObject() { }
-
     equals() { }
 
+    #next() {
+        // Takes the top element off of the sort list and binary sort into the main list
+        // This sets a new upper bound that we can't go above.
+        // Update the upper bound with new upper bound
+
+        // binary sort:
+        //      compare against middle element.
+        //      if main_list[middle] 'equal' target  then position = middle
+        //      if main_list[middle]  < target  then lower_bound = middle + 1;
+        //      if main_list[middle]  > target  then upper_bound = middle -1;
+        //      
+        //      if upper_bound < lower_bound  then target is smallest element
+        //      if lower_bound >= main_list.length  then target is largest element
+        //      if 
+        //          
+        // Takes the bottom element off of the sort list and binary sort into the main list. 
+        // Sets new lower bound that we can no longer go below. 
+        // Repeat until sorted in and apply to next list etc. 
+    }
+
+    #middle_index() { return Math.floor((this.lower_bound + this.upper_bound) / 2) }
 }
+
+
+
+let list1 = [[1], [2, 2], [3, 3, 3], [4, 4, 4, 4], [5, 5, 5, 5, 5]]
+let list2 = [['a'], ['b'], ['c'], ['d'], ['e']]
+
+let n = 2
+
+list1 = list1.reduce((acc, _, i, arr) => i % n === 0 ? [...acc, arr.slice(i, i + n).flat()] : acc, []).sort((a, b) => b.length - a.length);
+
+
+
+let low = 0;
+let high = 9;
+
+let middle = Math.floor((low + high) / 2)
+
+console.log(middle);
