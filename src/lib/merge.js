bc34b7babc274794b86b91fca731e45bf0f40fc3
Matt Tyler <tylma141@student.otago.ac.nz>
2023-10-11 07:01:54 +1300

    Bit of cleaning

--- src/lib/merge.js
+++ src/lib/merge.js
@@ -40,46 +40,72 @@ class Merge {
 
     }
 
-    static fromObject() { }
-    toObject() { }
-    equals() { }
+    static fromObject(obj) {
+        // Create a new instance
+        let mergeInstance = new Merge(obj.document_lists, undefined, undefined);  // Assuming the second and third arguments aren't required, or available from the object.
+
+        // Populate the instance's properties from the object
+        mergeInstance.main_list = obj.main_list;
+        mergeInstance.sort_list = obj.sort_list;
+        mergeInstance.current_upper = obj.current_upper;
+        mergeInstance.current_lower = obj.current_lower;
+        mergeInstance.list_upper = obj.list_upper;
+        mergeInstance.list_lower = obj.list_lower;
+        mergeInstance.middle = obj.middle;
+        mergeInstance.mode = obj.mode;
+        mergeInstance.current_sort_element = obj.current_sort_element;
+        mergeInstance.current_comparison_element = obj.current_comparison_element;
+        // ... set any other properties needed
+
+        return mergeInstance;
+    }
+
+    toObject() {
+        return {
+            document_lists: this.document_lists,
+            main_list: this.main_list,
+            sort_list: this.sort_list,
+            current_upper: this.current_upper,
+            current_lower: this.current_lower,
+            list_upper: this.list_upper,
+            list_lower: this.list_lower,
+            middle: this.middle,
+            mode: this.mode,
+            current_sort_element: this.current_sort_element,
+            current_comparison_element: this.current_comparison_element
+            // ... add any other relevant fields
+        };
+    }
+
+    equals(other) {
+        if (!(other instanceof Merge)) {
+            return false;
+        }
+
+        // Compare each field. Here's an example for some fields:
+        return this.document_lists === other.document_lists &&
+            this.main_list === other.main_list &&
+            this.sort_list === other.sort_list &&
+            this.current_upper === other.current_upper &&
+            this.current_lower === other.current_lower &&
+            // ... compare other fields similarly
+            this.current_comparison_element === other.current_comparison_element;
+    }
 
     #next() {
-
         this.middle = this.#middle_index()
         this.current_comparison_element = this.main_list[this.middle]
-
 
     }
 
     #next_sort_element() {
-        if (this.sort_list.length !== 0) {
-
-            if (this.mode === "TOP") {
-                this.current_sort_element = this.sort_list.pop();
-                this.mode = "BOT";
-            } else {
-                this.current_sort_element = this.sort_list.shift();
-                this.mode = "TOP";
-            }
-
-            this.current_upper = this.list_upper;
-            this.current_lower = this.list_lower;
-
-            this.middle = this.#middle_index();
-
-        } else if (this.document_lists.length !== 0) {
+        if (this.sort_list.length === 0 && this.document_lists.length !== 0) {
             this.sort_list = this.document_lists.pop();
-
             this.current_upper = this.main_list.length - 1;
             this.current_lower = 0;
+        }
 
-            this.list_upper = this.current_upper;
-            this.list_lower = this.current_lower
-
-            this.middle = this.#middle_index();
-
-            this.current_comparison_element = this.main_list[this.middle]
+        if (this.sort_list.length !== 0) {
             if (this.mode === "TOP") {
                 this.current_sort_element = this.sort_list.pop();
                 this.mode = "BOT";
@@ -87,7 +113,10 @@ class Merge {
                 this.current_sort_element = this.sort_list.shift();
                 this.mode = "TOP";
             }
-
+            this.current_upper = this.list_upper;
+            this.current_lower = this.list_lower;
+            this.middle = this.#middle_index();
+            this.current_comparison_element = this.main_list[this.middle];
         } else {
             console.log("GAME OVER MAN");
         }
@@ -97,28 +126,23 @@ class Merge {
 
 
     async greater_than() {
-        // this.current_sort_element > this.current_comparison_element
         this.current_upper = this.middle - 1;
 
         if (this.current_upper < this.current_lower) {
-            // insert at this.current_upper or this.current lower? 
-            this.main_list.splice(this.current_lower, 0, this.current_sort_element)
+            this.main_list.splice(this.current_lower, 0, this.current_sort_element);
 
-            if (this.mode == "TOP") {
+            if (this.mode === "TOP") {
                 this.list_upper = this.current_lower + 1;
             } else {
                 this.list_lower = this.current_lower - 1;
             }
 
             this.#next_sort_element();
-            // shift to next element adjusting the new list upper or lower bound.
-
-
         } else {
-
             this.#next();
         }
     }
+
 
     async equal() {
 
@@ -133,27 +157,24 @@ class Merge {
         this.#next_sort_element();
     }
 
-
     async less_than() {
-        // this.current_sort_element < this.current_comparison_element
         this.current_lower = this.middle + 1;
 
         if (this.current_lower > this.current_upper) {
-            // insert at this.current_lower
             this.main_list.splice(this.current_lower, 0, this.current_sort_element);
 
-            if (this.mode == "TOP") {
+            if (this.mode === "TOP") {
                 this.list_upper = this.current_lower + 1;
             } else {
                 this.list_lower = this.current_lower - 1;
             }
 
             this.#next_sort_element();
-
         } else {
             this.#next();
         }
     }
+
 
 }
 
