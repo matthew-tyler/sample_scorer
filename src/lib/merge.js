793fac357861581ef7155514c1ea213e5a55407b
Matt Tyler <tylma141@student.otago.ac.nz>
2023-10-10 17:29:17 +1300

    Maybe almost done?

--- src/lib/merge.js
+++ src/lib/merge.js
@@ -108,18 +108,19 @@ class Merge {
         // Takes the next largest as the sort list
         this.sort_list = document_lists.pop();
 
-        // Then sets an upper bound and a lower bound
-        this.global_upper_bound = this.main_list.length - 1;
-        this.global_lower_bound = 0;
 
-        this.upper = this.global_upper_bound;
-        this.lower = this.global_lower_bound;
 
-        this.comparison_index = this.#middle_index(this.upper, this.lower);
+        this.current_upper = this.main_list.length - 1;
+        this.current_lower = 0;
+
+        this.list_upper = this.current_upper;
+        this.list_lower = this.current_lower
+
+        this.middle = this.#middle_index();
         this.mode = "TOP";
 
-        this.current_comparison_element = this.main_list[this.comparison_index]
         this.current_sort_element = this.sort_list.shift();
+        this.current_comparison_element = this.main_list[this.middle]
 
     }
 
@@ -129,38 +130,89 @@ class Merge {
 
     #next() {
 
-        this.comparison_index = this.#middle_index(this.upper, this.lower)
-        this.current_comparison_element = this.main_list[this.comparison_index]
+        if (this.sort_list.length !== 0) {
 
-        // Takes the top element off of the sort list and binary sort into the main list
-        // This sets a new upper bound that we can't go above.
-        // Update the upper bound with new upper bound
-        //          
-        // Takes the bottom element off of the sort list and binary sort into the main list. 
-        // Sets new lower bound that we can no longer go below. 
-        // Repeat until sorted in and apply to next list etc. 
+            this.middle = this.#middle_index()
+            this.current_comparison_element = this.main_list[this.middle]
+
+
+
+        } else if (this.document_lists.length !== 0) {
+            this.sort_list = document_lists.pop();
+
+            this.current_upper = this.main_list.length - 1;
+            this.current_lower = 0;
+
+            this.list_upper = this.current_upper;
+            this.list_lower = this.current_lower
+
+            this.middle = this.#middle_index();
+            this.current_comparison_element = this.main_list[this.middle]
+            this.current_sort_element = this.sort_list.shift();
+        } else {
+            console.log("GAME OVER MAN");
+        }
+
     }
 
-    #middle_index(high, low) { return Math.floor((high + low) / 2) }
+
+    #middle_index() { return Math.floor((this.upper + this.lower) / 2) }
 
 
-    async greater_than() { }
+    async greater_than() {
+
+
+
+        // this.current_sort_element > this.current_comparison_element
+        this.current_upper = this.middle - 1;
+
+        // if sort comparison_element bigger than sort element then low bound must move to mid + 1.
+
+        if (this.current_upper < this.current_lower) {
+            // insert above this.middle
+
+            // shift to next element adjusting the new list upper or lower bound.
+
+            // if in TOP mode then the the list upper bound is equal to where this element is inserted -1
+
+            // if in BOT mode then the list lower bound is equal to where this element is inserted + 1 
+        } else {
+
+            this.#next();
+        }
+
+        // if sort_element is bigger than comparison element, then upper bound = mid - 1
+
+
+    }
 
     async equal() {
-        this.main_list[this.comparison_index].equal(this.current_sort_element);
+
+        this.main_list[this.middle].equal(this.current_sort_element);
+
         if (this.mode === "TOP") {
-            this.global_upper_bound = this.comparison_index;
+            this.list_upper = this.middle;
         } else {
-            this.global_lower_bound = this.comparison_index;
+            this.list_lower = this.middle;
+        }
+
+        this.mode = "BOT"
+        this.#next()
+    }
+
+    async less_than() {
+        this.current_lower = this.middle + 1;
+
+        if (this.current_upper > this.current_lower) {
+            // opposite of greater than.
+        } else {
+
+            this.#next();
         }
 
 
-        this.mode = "BOT"
     }
-
-    async less_than() { }
 }
-
 
 
 let list1 = [[1], [2, 2], [3, 3, 3], [4, 4, 4, 4], [5, 5, 5, 5, 5]]
@@ -172,8 +224,8 @@ list1 = list1.reduce((acc, _, i, arr) => i % n === 0 ? [...acc, arr.slice(i, i +
 
 
 
-let low = 0;
-let high = 9;
+let low = 4;
+let high = 4;
 
 let middle = Math.floor((low + high) / 2)
 
